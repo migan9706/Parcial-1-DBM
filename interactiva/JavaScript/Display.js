75f6a8489fd1bfc5c48ff3bd2187e5ce431d6b14
migan9706 <43798609+migan9706@users.noreply.github.com>
2019-02-15 13:50:40 -0600

    efectos js

--- interactiva/JavaScript/Display.js
+++ interactiva/JavaScript/Display.js
@@ -1,14 +1,23 @@
-function set(id,name) {
-	if(name=="Huawei"||name="Lanix"||name="Motorola"||name="Motorola"||name="Samsung"||name="Sony"||name="Xiaomi"){
-		getElementById(id).style.background='#857567';	
-		alert("entro aqui");
-	}
-	else
-	{
-			alert("entro aqui 2");
-		document.getElementById(id).style.height = "200px";
-		document.getElementById(id).style.width = "200px";
+function setCel(id) {
+	
+		document.getElementById(id).style.height = "100px";
+		document.getElementById(id).style.width = "100px";
 	
 	}	
-}
+function setCel1(id) {
+	
+		document.getElementById(id).style.height = "100%";
+		document.getElementById(id).style.width = "150px";
+	
+	}	
 
+
+
+function setMarca(id){
+		document.getElementById(id).style.height = "100px";
+		document.getElementById(id).style.width = "100px";
+}
+function setMarca1(id){
+		document.getElementById(id).style.height = "95%";
+		document.getElementById(id).style.width = "150px";
+}
