ee362e6cbeb8ecc6bfbf528b9e57221664e55d65
migan9706 <43798609+migan9706@users.noreply.github.com>
2019-02-16 00:54:00 -0600

    arreglo a colores.js

--- interactiva/JavaScript/colores.js
+++ interactiva/JavaScript/colores.js
@@ -2,11 +2,16 @@ function cargarcolor1()
 {
 	document.getElementById("imagen-colores1").src ="./Imagenes/icono/azultapa.png";
 	document.getElementById("imagen-colores1").style.visibility="visible";
+		document.getElementById("imagen-colores1").style.heigth="100%";
+		document.getElementById("imagen-colores1").style.width="100%";
+		document.getElementById("imagen-colores1").style.visibility="visible";
 	
 }
 function cargarcolor2()
 {
 		document.getElementById("imagen-colores1").src ="./Imagenes/icono/taparosa.png";
+		document.getElementById("imagen-colores1").style.width="100%";
+		document.getElementById("imagen-colores1").style.heigth="100%";
 		document.getElementById("imagen-colores1").style.visibility="visible";
 	
 }
@@ -14,11 +19,17 @@ function cargarcolor3()
 {
 	document.getElementById("imagen-colores2").src ="./Imagenes/icono/azultapa.png";
 	document.getElementById("imagen-colores2").style.visibility="visible";
+	document.getElementById("imagen-colores2").style.width="100%";
+	document.getElementById("imagen-colores2").style.heigth="100%";
+	document.getElementById("imagen-colores2").style.visibility="visible";
 	
 }
 function cargarcolor4()
 {
 		document.getElementById("imagen-colores2").src ="./Imagenes/icono/taparosa.png";
+		document.getElementById("imagen-colores2").style.visibility="visible";
+		document.getElementById("imagen-colores2").style.width="100%";
+		document.getElementById("imagen-colores2").style.heigth="100%";
 		document.getElementById("imagen-colores2").style.visibility="visible";
 	
 }
@@ -26,12 +37,16 @@ function cargarcolor4()
 function quitarcolor1()
 {
 	document.getElementById("imagen-colores1").style.visibility="hidden";
+	document.getElementById("imagen-colores1").style.width="0px";
+	document.getElementById("imagen-colores1").style.heigth="0px";
 	
 	
 }
 function quitarcolor2()
 {
 	document.getElementById("imagen-colores2").style.visibility="hidden";
+	document.getElementById("imagen-colores2").style.width="0px";
+	document.getElementById("imagen-colores2").style.heigth="0px";
 	
 	
 }
