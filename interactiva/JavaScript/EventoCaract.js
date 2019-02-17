dc62c39f301119ef2a84391352716bf468e260b1
rankuth <47220764+rankuth@users.noreply.github.com>
2019-02-17 00:17:36 -0600

    colocar informacion
    se colocaron imagenes que llevan la informacion de los celulares de unas  de sus caracteristicas para simular el trabajo  de comparar telefonos

--- interactiva/JavaScript/EventoCaract.js
+++ interactiva/JavaScript/EventoCaract.js
@@ -2,26 +2,26 @@
 
 function cargarinfo1(){
 
-	document.getElementById('cuadro2-1').style.background='#FBB678 url("informacion/info1.jpg") no-repeat right top';
-	document.getElementById('cuadro2-2').style.background='#FBB678 url("informacion/info2.jpg") no-repeat right top';
+	document.getElementById('cuadro2-1').style.background='#FBB678 url("Imagenes/informacion/cel1red.png") no-repeat right top';
+	document.getElementById('cuadro2-2').style.background='#FBB678 url("Imagenes/informacion/cel2red.png") no-repeat right top';
 }
 function cargarinfo2(){
-	document.getElementById('cuadro2-1').style.backgroundImage='#FBB678 url("informacion/info2.jpg") no-repeat right top';
-	document.getElementById('cuadro2-2').style.backgroundImage='#FBB678 url("informacion/info1.jpg") no-repeat right top';
+	document.getElementById('cuadro2-1').style.background='#FBB678 url("Imagenes/informacion/cel2ram.png") no-repeat right top';
+	document.getElementById('cuadro2-2').style.background='#FBB678 url("Imagenes/informacion/cel3ram.png") no-repeat right top';
 }
 function cargarinfo3(){
 
-	document.getElementById('cuadro2-1').style.backgroundImage='#FBB678 url("informacion/info1.jpg") no-repeat right top';
-	document.getElementById('cuadro2-2').style.backgroundImage='#FBB678 url("informacion/info2.jpg") no-repeat right top';
+	document.getElementById('cuadro2-1').style.background='#FBB678 url("Imagenes/informacion/cel4memoria.png") no-repeat right top';
+	document.getElementById('cuadro2-2').style.background='#FBB678 url("Imagenes/informacion/cel3memoria.png") no-repeat right top';
 }
 function cargarinfo4(){
 
-	document.getElementById('cuadro2-1').style.backgroundImage='#FBB678 url("informacion/info2.jpg") no-repeat right top';
-	document.getElementById('cuadro2-2').style.backgroundImage='#FBB678 url("informacion/info1.jpg") no-repeat right top';
+	document.getElementById('cuadro2-1').style.background='#FBB678 url("Imagenes/informacion/cel2camara.png") no-repeat right top';
+	document.getElementById('cuadro2-2').style.background='#FBB678 url("Imagenes/informacion/cel3camara.png") no-repeat right top';
 }
 function cargarinfo5(){
-	document.getElementById('cuadro2-1').style.backgroundImage='#FBB678 url("informacion/info1.jpg") no-repeat right top';
-	document.getElementById('cuadro2-2').style.backgroundImage='#FBB678 url("informacion/info2.jpg") no-repeat right top';
+	document.getElementById('cuadro2-1').style.background='#FBB678 url("Imagenes/informacion/cel4peso.png") no-repeat right top';
+	document.getElementById('cuadro2-2').style.background='#FBB678 url("Imagenes/informacion/cel3peso.png") no-repeat right top';
 
 }
 
