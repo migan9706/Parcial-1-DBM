a3da6de3184fbc7204df16cbd94e84dd28ccfad7
migan9706 <43798609+migan9706@users.noreply.github.com>
2019-02-18 19:11:48 -0600

    mejora al display

--- interactiva/JavaScript/Display.js
+++ interactiva/JavaScript/Display.js
@@ -14,10 +14,18 @@ function setCel1(id) {
 
 
 function setMarca(id){
-		document.getElementById(id).style.height = "100px";
-		document.getElementById(id).style.width = "100px";
+		document.getElementById(id).style.height = "75px";
+		document.getElementById(id).style.width = "75px";
 }
 function setMarca1(id){
 		document.getElementById(id).style.height = "95%";
-		document.getElementById(id).style.width = "15s0px";
+		document.getElementById(id).style.width = "150px";
+}
+function activopapelera1()
+{
+	document.getElementById("pap1").src="./Imagenes/icono/papelera-activa.png";
+}
+function activopapelera2()
+{
+	document.getElementById("pap2").src="./Imagenes/icono/papelera-activa.png";
 }
