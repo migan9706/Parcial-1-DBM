517b37ee44cdf9e1f6c22803707fa363c9d5c45e
migan9706 <43798609+migan9706@users.noreply.github.com>
2019-02-18 18:23:47 -0600

    arraglando display tam heigh and weight

--- interactiva/JavaScript/Display.js
+++ interactiva/JavaScript/Display.js
@@ -1,13 +1,13 @@
 function setCel(id) {
 	
 		document.getElementById(id).style.height = "100px";
-		document.getElementById(id).style.width = "50px";
+		document.getElementById(id).style.width = "82px";
 	
 	}	
 function setCel1(id) {
 	
 		document.getElementById(id).style.height = "120px";
-		document.getElementById(id).style.width = "80px";
+		document.getElementById(id).style.width = "100px";
 	
 	}	
 
@@ -19,5 +19,5 @@ function setMarca(id){
 }
 function setMarca1(id){
 		document.getElementById(id).style.height = "95%";
-		document.getElementById(id).style.width = "150px";
+		document.getElementById(id).style.width = "15s0px";
 }
